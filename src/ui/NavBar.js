a181a76cfe603754d4a6bdcb5e2d05b061eb4e60
Titan antony <antonytitan96@gmail.com>
2024-11-19 18:53:24 +0300

    Chore: added a menu bar for smaller screens

--- src/ui/NavBar.js
+++ src/ui/NavBar.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import './NavBar.css'
-import logo from '../images/logo.png'
+import React, { useState } from 'react';
+import './NavBar.css';
+import logo from '../images/logo.png';
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-   <nav className="navbar">
+    <nav className="navbar">
       <div className="logo">
         <a href='#top'>
-        <img src={logo} alt="Athleticon Logo" className="logo-image" />
+          <img src={logo} alt="Athleticon Logo" className="logo-image" />
         </a>
       </div>
-      <ul className="nav-links">
+      <div className="menu-icon" onClick={toggleMenu}>
+        {/* You can replace this div with an icon or SVG */}
+        &#9776;
+      </div>
+      <ul className={`nav-links ${menuOpen ? 'show' : ''}`}>
         <li><a href="#home">Home</a></li>
         <li><a href="#exercise">Exercise</a></li>
         <li><a href="#features">Features</a></li>
@@ -24,7 +34,7 @@ function NavBar() {
         <li><a href="#about">About</a></li>
       </ul>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
