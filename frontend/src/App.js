7d4acf73c6027baee7b5585259ef670fa4c6e9d8
Titan antony <antonytitan96@gmail.com>
2024-11-27 15:03:24 +0300

    Chore: add js components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,19 +14,20 @@ import HeroSection from './ui/HeroSection/HeroSection';
 import image1 from "./images/1.jpeg";
 import image2 from "./images/2.jpeg";
 import image3 from "./images/3.jpeg";
-// import fourthImage from "./images/4.png";
+import image4 from "./images/4.jpeg";
+import image5 from "./images/5.jpeg";
+import image6 from "./images/6.jpeg";
+import Routinecards from "./ui/Routinecard/Routinecards";
+import ImageGallery from "./ui/SlideShow/ImageGallery";
 
 
+const photos = [image4, image5, image6];
 
-// const images = [
-//   { src: runnerImage, alt: "Runner in motion" },
-//   { src: secondImage, alt: "Second image description" },
-//   { src: thirdImage, alt: "Third image description" },
-//   { src: fourthImage, alt: "Fourth image description" },
-// ];
-
-
-const images = [image1, image2, image3];
+const images = [
+    { src: image1, alt: "Image 1" },
+    { src: image2, alt: "Image 2" },
+    { src: image3, alt: "Image 3" },
+  ];
 
 const theme = {
   colors: {
@@ -49,9 +50,11 @@ function App() {
       title="Move to Maintain your Health"
       subtitle="Energize your lifestyle, embrace movement for a healthier you. Get motivated, stay active, and reap the benefits of a vibrant, well-nurtured body."
       buttonText="Get Started"
-      images={images}
+      images={photos}
     />
-    
+    <Routinecards />
+    <ImageGallery images={images} />
+
     
     </ThemeProvider>
     
